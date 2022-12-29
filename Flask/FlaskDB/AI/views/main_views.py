46bf1d182624b893604a41165fbc2319ceae7559
extremek2 <extremek2@paran.com>
2022-12-29 19:05:30 +0900

    map = main_views.py 수정 >> map.html 추가

--- Flask/FlaskDB/AI/views/main_views.py
+++ Flask/FlaskDB/AI/views/main_views.py
@@ -23,9 +23,12 @@ def faq():
     return render_template('faq.html')
 
 # 챗봇
-@bp.route('/chatbot', methods=['POST'])
+@bp.route('/chatbot2', methods=['POST'])
 def chatbot():
     result = request.get_json()
     print('영화 제목 {}'.format(result['queryResult']['parameters']['movie_name']))
     return {'fulfillmentText': '영화 내용을 알려줄까 말까 ?'}
 
+@bp.route('/map', methods=['GET', 'POST'])
+def map():
+    return render_template('map.html')
