7f3e52166387d7a277f887900338ef0f53581a9b
Geun <kkoowl99@gmail.com>
2023-01-03 21:01:08 +0900

    플라스크 커밋 로그인,회원가입

--- Flask/FlaskDB/AI/forms.py
+++ Flask/FlaskDB/AI/forms.py
@@ -3,19 +3,19 @@ from wtforms import StringField, TextAreaField , PasswordField , EmailField
 from wtforms.validators import DataRequired , Length , EqualTo , Email
 
 class UserCreateForm(FlaskForm):
-    username = StringField('사용자이름', validators=[DataRequired(), Length(min=3, max=20)])
-    password1 = PasswordField('비밀번호', validators=[DataRequired(), EqualTo('password2', '비밀번호가 다릅니다.')])
-    password2 = PasswordField('비밀번호 확인', validators=[DataRequired()])
-    name = StringField('이름' , validators=[DataRequired() , Length(min=3 , max=20)])
-    birthday = StringField('생년월일' , validators=[DataRequired() , Length(min=8 , max=8)])
-    gender = StringField("성별" , validators=[DataRequired() , Length(min=1 , max=8)])
-    address1 = StringField('주소' , validators=[DataRequired()])
-    address2 = StringField('상세주소' , validators=[DataRequired()])
-    email = EmailField('이메일', validators=[DataRequired(), Email()])
-    phone = StringField('전화번호', validators=[DataRequired()])
+    username = StringField('사용자이름', validators=[DataRequired("ID를 확인해주세요"), Length(min=3, max=25)]) # Length 오류 메세지 찾는중 + 이메일 / 폰 예외처리
+    password1 = PasswordField('비밀번호', validators=[DataRequired("비밀번호를 확인해주세요"), Length(min=8, max=20) , EqualTo('password2', '비밀번호가 다릅니다.')])
+    password2 = PasswordField('비밀번호 확인', validators=[DataRequired() , Length(min=8, max=20)])
+    name = StringField('이름' , validators=[DataRequired("이름을 확인해주세요") , Length(min=3 , max=20)])
+    birthday = StringField('생년월일' , validators=[DataRequired("생년월일을 확인해주세요") , Length(min=8 , max=8)])
+    gender = StringField("성별" , validators=[DataRequired("성별을 확인해주세요") , Length(min=1 , max=8)])
+    address1 = StringField('주소' , validators=[DataRequired("주소를 확인해주세요")])
+    address2 = StringField('상세주소' , validators=[DataRequired("상세주소를 확인해주세요")])
+    email = EmailField('이메일', validators=[DataRequired("이메일을 확인해주세요"), Email()])
+    phone = StringField('전화번호', validators=[DataRequired("전화번호를 확인해주세요")])
 
 class UserLoginForm(FlaskForm):
-    username = StringField('사용자이름' , validators=[DataRequired('ID를 확인해주세요'), Length(min=3 , max=25)])
+    username = StringField('유저 이름' , validators=[DataRequired('ID를 확인해주세요'), Length(min=3 , max=25)])
     password = PasswordField('비밀번호' , validators=[DataRequired('비밀번호를 확인해주세요')])
 
 class QuestionForm(FlaskForm):
